2ca418194e4eb1c0824581e35df7b9462d6ccb6d
ethan.jung <ethan.jung@musinsa.com>
2022-03-09 18:07:10 +0900

    add Filter

---
+++ src/main/java/com/musinsa/graphql/filter/GraphQLFilter.java
@@ -0,0 +1,21 @@
+package com.musinsa.graphql.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+@WebFilter(urlPatterns = "/*")
+public class GraphQLFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("filter run");
+        chain.doFilter(request, response);
+    }
+}
